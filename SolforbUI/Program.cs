08ba8b01407f136eb48d68316caabf9fe41289a7
PogorelovSemyon <Semen.Pogorelov@mechel.com>
2023-11-09 12:56:30 +0700

    Initialize Project

---
+++ SolforbUI/Program.cs
@@ -0,0 +1,62 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace SolforbUI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Rubius API",
+                    Description = "An ASP.NET Core Web API for managing Rubius items"
+                });
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            });
+
+            // Add services to the container.
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    options.RoutePrefix = string.Empty;
+                });
+            }
+
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller}/{action}/{id?}");
+
+            app.MapFallbackToFile("index.html");
+
+            app.Run();
+        }
+    }
+}
